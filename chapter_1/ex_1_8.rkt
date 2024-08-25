2fba57c5a843b31e7759f7b9ad7bd25a2b92a28d
Bobby Nelson <rnelsonxy@gmail.com>
2024-08-25 10:49:37 -0400

    Improves Exercise 1.8

--- chapter_1/ex_1_8.rkt
+++ chapter_1/ex_1_8.rkt
@@ -1,11 +1,22 @@
 #lang sicp
 
+
+;;; Compute the absolute value of x.
+;;; This is sufficiently abstracted to be in its own procudure.
 (define (abs x)
   (if (>= x 0)
       x
       (- x)))
 
-(define (cube-root guess x)
+#|
+Compute the cube-root of x using Newton's method.
+
+Note that there are multiple internal procedures that are not
+abstracted enough to be taken out of the cube-root scope. These
+procedures are tightly coupled to the cube-root procedure. As
+mentioned in the book, we will improve the abstraction later.
+|#
+(define (cube-root x)
   (define tolerance 0.001)
   (define (good-enough? guess x)
     (< (abs (- (expt guess 3)
@@ -15,6 +26,8 @@
     (/ (+ (/ x (expt guess 2))
           (* 2 guess))
        3))
-  (if (good-enough? guess x)
+  (define (cube-root-iter guess x)
+    (if (good-enough? guess x)
       guess
-      (cube-root (improve guess x) x)))
+      (cube-root-iter (improve guess x) x)))
+  (cube-root-iter 1.1 x))
