68763e71da46e4c522e83f65ee141a930dccdb25
Bobby Nelson <rnelsonxy@gmail.com>
2025-02-22 16:36:21 -0500

    Simplified Ex 1.39

--- chapter_1/ex_1_39.rkt
+++ chapter_1/ex_1_39.rkt
@@ -1,14 +1,5 @@
 #lang sicp
 
-
-(define (n-tan x i)
-  (if (= i 1)
-      x
-      (* x x -1)))
-
-(define (d-tan i)
-  (- (* 2 i) 1))
-
 (define (cont-frac n d k)
   (define (cont-frac-i i)
     (if (= i k)
@@ -18,9 +9,13 @@
   (cont-frac-i 1))
 
 (define (tan-cf x k)
-  (cont-frac (lambda (i) (n-tan x i))
-             d-tan
-             k))
+  (define (n x i)
+    (if (= i 1)
+        x
+        (* x x -1)))
+  (define (d i)
+    (- (* 2 i) 1))
+  (cont-frac (lambda (i) (n x i)) d k))
 
 (define test-values '(0.0001 3.141592 2 10))
 (map (lambda (x) (exact->inexact (tan-cf x 20))) test-values)
