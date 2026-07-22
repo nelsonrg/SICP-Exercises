f88bfdae79e266e85f333da745e8187399e82c0b
Bobby Nelson <rnelsonxy@gmail.com>
2026-07-21 21:26:18 -0400

    Exercise 2.45

--- chapter_2/painting_exercises.rkt
+++ chapter_2/painting_exercises.rkt
@@ -1,12 +1,6 @@
 #lang sicp
 (#%require sicp-pict)
 
-(define (right-split painter n)
-  (if (= n 0)
-      painter
-      (let ((smaller (right-split painter (- n 1))))
-        (beside painter (below smaller smaller)))))
-
 (define (corner-split painter n)
   (if (= n 0)
       painter
@@ -19,13 +13,30 @@
                   (below bottom-right corner))))))
 
 ;; Exercise 2.44
-(define (up-split painter n)
+(define (up-split-old painter n)
   (if (= n 0)
       painter
-      (let ((smaller (up-split painter (- n 1))))
+      (let ((smaller (up-split-old painter (- n 1))))
         (below painter (beside smaller smaller)))))
 
 (display "Exercise 2.44 - up-split")
 (newline)
+(paint (up-split-old einstein 1))
+
+;; Exercise 2.45
+(define (split preposition1 preposition2)
+  (lambda (painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller ((split preposition1 preposition2) painter (- n 1))))
+          (preposition1 painter (preposition2 smaller smaller))))))
+
+(define right-split (split beside below))
+(define up-split (split below beside))
+
+(display "Exercise 2.45 - split")(newline)
+(display "right-split")(newline)
+(paint (right-split einstein 1))
+(display "up-split")(newline)
 (paint (up-split einstein 1))
 
